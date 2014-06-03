555c8f7ee95e5114a4b871ad2473c9c755d73b57
Josef <josefmonje@gmail.com>
2014-06-03 19:50:53 +0800

    db.py

--- db.py
+++ db.py
@@ -1,5 +1,5 @@
 import MySQLdb, hashlib, cgi, cgitb; cgitb.enable()
-import FieldEmployees
+import FieldEmployees, Clients, Detachments
 
 mysql = MySQLdb.connect('localhost','AdminPayroll','Password','Eaglewatch')
 cur = mysql.cursor()
@@ -49,6 +49,11 @@ def List(tableName):
   res = getAll(sql)
   return res
 
+def SubList(tableName, foreignKey, value):
+  sql = "SELECT * FROM %s where %s = %s" % (tableName, foreignKey, value)
+  res = getAll(sql)
+  return res
+
 def getAllFieldEmployees():
   #sql = "SELECT * FROM FieldEmployees"
   #res = getAll(sql)
@@ -62,41 +67,28 @@ def getAllFieldEmployees():
       row = cur.fetchone()
   return FieldEmployeeList
 
-def getAllDetachments(clientID):
-  #sql = "SELECT * FROM FieldEmployees"
-  #res = getAll(sql)
-  res = List("Detachments")
-  FieldEmployeeList = []
-  for row in res:
-    if row is not None:
-      #FieldEmployeeList.append([ str(row[0]), str(row[1]), str(row[2]), str(row[3]), str(row[4]), str(row[5]), str(row[6]), str(row[7]), str(row[8]), str(row[9]), str(row[10]), str(row[11]), str(row[12]), str(row[13]), str(row[14]), str(row[15]), str(row[16]), str(row[17]), str(row[18]), str(row[19]), str(row[20]) ])
-      FieldEmployee = FieldEmployees.FieldEmployees( str(row[0]), str(row[1]), str(row[2]), str(row[3]), str(row[4]), str(row[5]), str(row[6]), str(row[7]), str(row[8]), str(row[9]), str(row[10]), str(row[11]), str(row[12]), str(row[13]), int(row[14]), str(row[15]), str(row[16]), str(row[17]), str(row[18]), str(row[19]), str(row[20]) )
-      FieldEmployeeList.append(FieldEmployee)
-      row = cur.fetchone()
-  return FieldEmployeeList
-
 def getAllClients():
+  res = List("Clients")
+  ClientList = []
+  for row in res:
+    if row is not None:
+      Client = Clients.Clients( int(row[0]), str(row[1]), str(row[2]), str(row[3]), str(row[4]), str(row[5]) )
+      ClientList.append(Client)
+      row = cur.fetchone()
+  return ClientList
+
+def getAllDetachments(val):
   #sql = "SELECT * FROM FieldEmployees"
   #res = getAll(sql)
-  res = List("Clients")
-  FieldEmployeeList = []
+  res = SubList("Detachments", "ClientID", val)
+  DetachmentList = []
   for row in res:
     if row is not None:
       #FieldEmployeeList.append([ str(row[0]), str(row[1]), str(row[2]), str(row[3]), str(row[4]), str(row[5]), str(row[6]), str(row[7]), str(row[8]), str(row[9]), str(row[10]), str(row[11]), str(row[12]), str(row[13]), str(row[14]), str(row[15]), str(row[16]), str(row[17]), str(row[18]), str(row[19]), str(row[20]) ])
-      FieldEmployee = FieldEmployees.FieldEmployees( str(row[0]), str(row[1]), str(row[2]), str(row[3]), str(row[4]), str(row[5]), str(row[6]), str(row[7]), str(row[8]), str(row[9]), str(row[10]), str(row[11]), str(row[12]), str(row[13]), int(row[14]), str(row[15]), str(row[16]), str(row[17]), str(row[18]), str(row[19]), str(row[20]) )
-      FieldEmployeeList.append(FieldEmployee)
+      Detachment = Detachments.Detachments( int(row[0]), int(row[1]), int(row[2]), str(row[3]), str(row[4]), str(row[5]), str(row[6]), str(row[7]), str(row[8]), str(row[9]) )
+      DetachmentList.append(Detachment)
       row = cur.fetchone()
-  return FieldEmployeeList
+  return DetachmentList
 
-def getOneFieldEmployee():
-  sql = "SELECT * FROM FieldEmployees WHERE ID = %s"
-  params = ("1")
-  res = getOne(sql, params)
-  res = str(res)
-  FieldEmployees = []
-  ctr = 0
-  for row in res:
-    FieldEmployees[ctr] = [ str(row[0]), str(row[1]), str(row[2]), str(row[3]), str(row[4]), str(row[5]), str(row[6]), str(row[7]), str(row[8]), str(row[9]), str(row[10]), str(row[11]), str(row[12]), str(row[13]), str(row[14]), str(row[15]), str(row[16]), str(row[17]), str(row[18]), str(row[19]), str(row[20]) ]
-    ctr = ctr + 1
-  print FieldEmployees[0]
-
+#def getContactPersons(of client):
+#def getContactPersons(of detachment):
