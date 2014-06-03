6246a38f24182e9ebef7a3ddb8060ad0f4179233
nadinejamila <nadinejamila@yahoo.com>
2014-06-04 02:48:34 +0800

    working benefits template

--- views.py
+++ views.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, session, escape, request, redirect, url_for
-import db
 app = Flask(__name__)
 app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 
@@ -33,7 +32,7 @@ def login():
         return redirect(url_for('listDetachmentsPayables'))
     if user == 'benefits_officer':
         session['usertype'] = 'BeO'
-        return redirect(url_for('viewPeriodsBenefits'))
+        return redirect(url_for('viewLoansSSS'))
     else:
       return redirect(url_for(user))
   return render_template('login.html')
@@ -49,11 +48,22 @@ def admin(user=None):
 
 @app.route('/employees', methods=['POST', 'GET'])
 def listEmployees(user=None):
-  return render_template('employee_search.html', FEs = db.getAllFieldEmployees(), user=escape(session['user']))
+  field_employee1 = FieldEmployee(1,'SG', 'FE001234', 'Jr.', "D'Agosto", 'Nicholas', 'Smith', '9323232', '09178287292', 'Commonwealth, Q.C.', '02-03-1989', 'M', 'S', 0, 'driving', '01/02/2014', '', 'resigned', 'C', 'E')
+  field_employee2 = FieldEmployee(2,'SG', 'FE002345', 'IV', 'Naharis', 'Dario', 'Sunter', '9323232', '09178287292', 'Commonwealth, Q.C.', '02-03-1989', 'M', 'S', 0, 'driving', '01/02/2014', '', 'active', 'C', 'E')
+  field_employee3 = FieldEmployee(3,'SV', 'FE003456', 'Jr.', 'Hoult', 'Nick', 'Smith', '9323232', '09178287292', 'Commonwealth, Q.C.', '02-03-1989', 'M', 'S', 0, 'driving', '01/02/2014', '', 'resigned', 'C', 'E')
+  field_employee4 = FieldEmployee(4,'SV', 'FE003456', 'Jr.', 'Lannister', 'Jamie', 'Targaryen', '9323232', '09178287292', 'Commonwealth, Q.C.', '02-03-1989', 'M', 'S', 0, 'driving', '01/02/2014', '', 'awol', 'C', 'E')
+  field_employees = [field_employee1, field_employee2, field_employee3, field_employee4]
+  #field_employees = getAllFieldEmployees()
+  return render_template('employee_search.html', FEs = field_employees, user=escape(session['user']))
   
 @app.route('/employees/get/<ID>', methods=['POST', 'GET'])
 def viewEmployee(ID, user=None):
-  return render_template('employee.html', FE = db.getEmployee(ID), user=escape(session['user']), )
+    #field_employee= getEmployee(ID)
+  if ID == '1':
+    field_employee = FieldEmployee(1,'SG', 'FE001234', 'Jr.', "D'Agosto", 'Nicholas', 'Smith', '9323232', '09178287292', 'Commonwealth, Q.C.', '02-03-1989', 'M', 'S', 0, 'driving', '01/02/2014', '', 'resigned', 'C', 'E')
+  else:
+    field_employee = FieldEmployee(1,'SG', 'FE001234', 'Jr.', "Hoult", 'Nicholas', 'Smith', '9323232', '09178287292', 'Commonwealth, Q.C.', '02-03-1989', 'M', 'S', 0, 'driving', '01/02/2014', '', 'resigned', 'C', 'E')
+  return render_template('employee.html', FE = field_employee, user=escape(session['user']), )
 
 @app.route('/employees/add', methods=['POST', 'GET'])
 def addFieldEmployee(user=None):
@@ -145,9 +155,17 @@ def viewPayables(user=None):
 
 #BENEFITS ---TO BE ADDED -----
 
+@app.route('/benefits/SSS', methods=['POST', 'GET'])
+def viewLoansSSS(user=None):
+    return render_template('loan_SSS.html', user=escape(session['user']))
 
+@app.route('/benefits/pagibig_calamity', methods=['POST', 'GET'])
+def viewLoansCalamity(user=None):
+    return render_template('loan_PagibigCalamity.html', user=escape(session['user']))
 
-
+@app.route('/benefits/pagibig_salary', methods=['POST', 'GET'])
+def viewLoansSalary(user=None):
+    return render_template('loan_PagibigSalary.html', user=escape(session['user']))
 
 if __name__ == '__main__':
     app.debug = True
